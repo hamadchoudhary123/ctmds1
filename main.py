9d5c90ad1149d3085dbaa7f5c0c2a155d93596ce
hamad <hamadchoudhary@hotmail.co.uk>
2025-05-11 16:34:44 +0100

    Defines a function to generate a list of function

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import random
 import sys
 
-if __name__ == '__main__':
-    n = int(sys.argv[1])
+def generate_price_curve(n: int) -> list[float] :
+    prices = []
     for i in range(n):
         price = random.random() * 100
+        prices.append(price)
+    return prices
+
+if __name__ == '__main__':
+    n = int(sys.argv[1])
+    prices = generate_price_curve(n)
+    for i, price in enumerate(prices):
         print(f"{i}: {price:.2f} USD")
 
 
