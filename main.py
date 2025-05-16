80fec8c14d00a6ab8c2bcbb260aaf315f9010c4c
hamad <hamadchoudhary@hotmail.co.uk>
2025-05-16 14:15:10 +0100

    accepts houlry or half hourly granularity

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import sys
 
-def generate_price_curve(n: int) -> list[float] :
+def generate_price_curve(n: int) -> list[float]:
     prices = []
     for i in range(n):
         price = random.random() * 100
@@ -9,10 +9,8 @@ def generate_price_curve(n: int) -> list[float] :
     return prices
 
 if __name__ == '__main__':
-    n = int(sys.argv[1])
+    granularity = sys.argv[1]
+    n = 48 if granularity == 'hh' else 24
     prices = generate_price_curve(n)
     for i, price in enumerate(prices):
         print(f"{i}: {price:.2f} USD")
-
-
-
