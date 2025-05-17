5acc32e81ed17937c7fbf6e8977950c79bf97466
hamad <hamadchoudhary@hotmail.co.uk>
2025-05-17 15:51:17 +0100

    add support for user defined currencies

--- main.py
+++ main.py
@@ -5,18 +5,27 @@ import typer
 app = typer.Typer()
 
 def generate_price_curve(n: int) -> list[float]:
-    prices = []
-    for i in range(n):
-        price = random.random() * 100
-        prices.append(price)
-    return prices
+    return [random.random() * 100 for i in range(n)]
 
 @app.command()
-def pricecurve(granularity: str = typer.Option("h", "--granularity", "-g", help="Granularity: h (hourly) or hh (half-hourly)")):
+def pricecurve(
+        granularity: str = typer.Option("h", "--granularity", "-g", help="Granularity: h (hourly) or hh (half-hourly)"),
+        currency: str = typer.Option("USD", "--currency", "-c", help="Currency code e.g: USD or EUR"),
+):
+
     n = 48 if granularity == 'hh' else 24
     prices = generate_price_curve(n)
     for i, price in enumerate(prices):
-        print(f"{i}: {price:.2f} USD")
+        if granularity == 'hh':
+            total_minutes = i * 30
+        else:
+            total_minutes = i * 60
+
+        hours = total_minutes // 60
+        minutes = total_minutes % 60
+        time_label = f"{hours:02}{minutes:02}"
+
+        print(f"{time_label}: {price:.2f} {currency}")
 
 if __name__ == '__main__':
     app()
